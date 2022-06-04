5cdefb2e34c7f4d8f5b525bda4d6055063fe9ed2
Pablo Gorgoglione <pablogorgoglione@gmail>
2022-06-04 19:59:13 -0300

    final test onecommand

--- backend/utils/seed.ts
+++ backend/utils/seed.ts
@@ -1,13 +1,8 @@
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import Category from '../models/categoryModel';
 
 const seedCategory = async () => {
-  let mongoDB =
-    process.env.NODE_ENV == 'dev'
-      ? (process.env.DB_DEV as string)
-      : (process.env.DB_PRO as string);
-
+  let mongoDB = 'mongodb://localhost:27017/ensolvers_dev';
   try {
     await mongoose.connect(mongoDB);
     await Category.deleteMany();
@@ -16,15 +11,12 @@ const seedCategory = async () => {
       Category.create({ name: 'Personal' }),
       Category.create({ name: 'Important' }),
     ]);
-    process.env.NODE_ENV == 'dev'
-      ? console.log('Seeded dev db')
-      : console.log('Seeded production db');
+    console.log('Seeded production db');
   } catch (error) {
     console.log(error);
   }
 };
 
-dotenv.config();
 seedCategory().then(() => {
   mongoose.disconnect();
   process.exit(0);
