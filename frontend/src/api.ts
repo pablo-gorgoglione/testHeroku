e2010d5350654df56faaf63641ccaec36974e412
Pablo Gorgoglione <pablogorgoglione@gmail>
2022-06-04 13:54:14 -0300

    intentando hacer que funcione con un comando

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -6,8 +6,8 @@ const http = axios.create({
     'Content-type': 'application/json',
   },
 });
-const apiUrl = 'https://ensolvers-pablo-gorgoglione.herokuapp.com/api';
-// const apiUrl = 'http://localhost:4000/api';
+// const apiUrl = 'https://ensolvers-pablo-gorgoglione.herokuapp.com/api';
+const apiUrl = 'http://localhost:4000/api';
 const userEndPoint = `${apiUrl}/users`;
 
 export const userApi = {
